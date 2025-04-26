fe019bedf99e63f1ff65d098d93f4b7e1cd25ffc
ViA8604 <rolikvia8@gmail.com>
2025-04-26 13:04:41 -0400

    MenuPrefabResource

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuButtons : MonoBehaviour
 {
+    public GameObject menuPanelPrefabInstance; // Referencia al prefab MenuPanel
+
     public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    
+    public void Options()
+    {
+        if (menuPanelPrefabInstance == null)
         {
-            SceneManager.LoadScene("GameScene");
+            GameObject menuPanelPrefab = Resources.Load<GameObject>("Prefabs/MenuPanel"); // Carga el prefab desde la carpeta Resources
+            menuPanelPrefabInstance = Instantiate(menuPanelPrefab, GameObject.Find("Canvas").transform); // Instancia el prefab como hijo del canvas para que se muestre
         }
+        menuPanelPrefabInstance.SetActive(true); // Activa el panel de opciones
+    }
     
     public void ExitGame()
-        {
-            Application.Quit();
-        }
+    {
+        Application.Quit();
+    }
 }
