9e578bb8307d6feb98d2c8b2b9d568606c1d4d74
ViA8604 <rolikvia8@gmail.com>
2025-04-26 15:52:46 -0400

    feat\ScreenManager and ContinuousMap

--- Assets/Scripts/BasicMovePlayer.cs
+++ Assets/Scripts/BasicMovePlayer.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class BasicMovePlayer : MonoBehaviour
 {
     private Rigidbody2D body;
-    private float moveSpeed = 10f;
+    private float moveSpeed = 8f;
     private float jumpSpeed = 6f;
     private int jumpCount = 0; // Contar el numero de saltos seguidos
+    private bool hasHandledRightEdge = false; // Nueva bandera
 
     // Start is called before the first frame update
     void Awake()
@@ -33,14 +34,26 @@ public class BasicMovePlayer : MonoBehaviour
 
     void DontExceedLimits() //Para restringir que el jugador no salga de la pantalla
     {
-        // Restringir la posición del jugador dentro de los límites de la pantalla
         Vector3 playerPosition = transform.position;
         float screenHalfWidth = Camera.main.orthographicSize * Camera.main.aspect; // Ancho de la mitad de la pantalla
         float screenLeftLimit = Camera.main.transform.position.x - screenHalfWidth;
         float screenRightLimit = Camera.main.transform.position.x + screenHalfWidth;
 
-        // Limitar la posición horizontal del jugador
+        // Limitar la posición horizontal del jugador al borde izquierdo
         playerPosition.x = Mathf.Clamp(playerPosition.x, screenLeftLimit, screenRightLimit);
+
+        // Llamar a HandleRightScreenEdge si el jugador está en el borde derecho y no se ha manejado aún
+        if (playerPosition.x >= screenRightLimit && !hasHandledRightEdge)
+        {
+            HandleRightScreenEdge(ref playerPosition); // Pasar por referencia para que no de bateo
+            hasHandledRightEdge = true; // Marcar como manejado
+        }
+
+        // Restablecer la bandera si el jugador ya no está en el borde derecho
+        if (playerPosition.x < screenRightLimit)
+        {
+            hasHandledRightEdge = false;
+        }
 
         // Limitar la posición vertical del jugador (opcional, si es necesario)
         float screenBottomLimit = Camera.main.transform.position.y - Camera.main.orthographicSize;
@@ -48,6 +61,14 @@ public class BasicMovePlayer : MonoBehaviour
         playerPosition.y = Mathf.Clamp(playerPosition.y, screenBottomLimit, screenTopLimit);
 
         transform.position = playerPosition;
+    }
+
+    void HandleRightScreenEdge(ref Vector3 playerPosition)
+    {   
+        //Actualiza la escena y coloca al jugador al inicio
+        GameObject.FindWithTag("SceneManager").GetComponent<UpdateScene>().LoadNewerScene();
+        playerPosition = new Vector3(-8.27f, -2.76f, 0f);
+        transform.position = playerPosition; // Actualiza la posición del jugador
     }
 
     void OnCollisionEnter2D(Collision2D collision)
