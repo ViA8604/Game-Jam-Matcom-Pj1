6daf7e9148756857b23604a50ea3dc252d94e224
ViA8604 <rolikvia8@gmail.com>
2025-04-26 19:37:11 -0400

    WorkingPauseMenuDraft

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class MenuButtons : MonoBehaviour
 {
-    public GameObject menuPanelPrefabInstance; // Referencia al prefab MenuPanel
+    GameObject menuPanelPrefabInstance; // Referencia al prefab MenuPanel
+    GameObject infoPanelPrefabInstance; // Referencia al prefab InfoPanel
+    GameObject pausePanelPrefabInstance; // Referencia al prefab PausePanel
 
     public void PlayGame()
     {
@@ -20,7 +23,45 @@ public class MenuButtons : MonoBehaviour
         }
         menuPanelPrefabInstance.SetActive(true); // Activa el panel de opciones
     }
+
+    public void ShowThanksInfo()
+    {
+        if(infoPanelPrefabInstance == null)
+        {
+            GameObject infoPanelPrefab = Resources.Load<GameObject>("Prefabs/InfoPanel"); // Carga el prefab desde la carpeta Resources
+            infoPanelPrefabInstance = Instantiate(infoPanelPrefab, GameObject.Find("Canvas").transform); // Instancia el prefab como hijo del canvas para que se muestre
+
+            Button thanksButton = infoPanelPrefabInstance.GetComponentInChildren<Button>();
+            thanksButton.onClick.AddListener(() => thanksButton.transform.parent.gameObject.SetActive(false));
+        }
+        else
+        {
+        infoPanelPrefabInstance.SetActive(true);
+        }
+    }
     
+    public void PauseGame()
+    {
+        if(infoPanelPrefabInstance == null)
+        {
+            GameObject pausePanelPrefab = Resources.Load<GameObject>("Prefabs/PauseMenu"); // Carga el prefab desde la carpeta Resources
+            pausePanelPrefabInstance = Instantiate(pausePanelPrefab, GameObject.Find("Canvas").transform); // Instancia el prefab como hijo del canvas para que se muestre
+        
+            Button resumeButton = pausePanelPrefabInstance.transform.Find("ResumeButton").GetComponent<Button>();
+            resumeButton.onClick.AddListener(() => pausePanelPrefabInstance.SetActive(false));
+
+            Button returnToM = pausePanelPrefabInstance.transform.Find("ReturnMButton").GetComponent<Button>();
+            returnToM.onClick.AddListener(() => SceneManager.LoadScene("MainScene"));
+
+            Button exitGButton = pausePanelPrefabInstance.transform.Find("ExitGButton").GetComponent<Button>();
+            exitGButton.onClick.AddListener(ExitGame);
+        }
+
+        else
+        {
+        infoPanelPrefabInstance.SetActive(true);
+        }
+    }
     public void ExitGame()
     {
         Application.Quit();
