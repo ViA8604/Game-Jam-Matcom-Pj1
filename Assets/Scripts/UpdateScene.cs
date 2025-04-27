8a8b0d27ddcd672009bfb222e3d7c9339ff89921
ViA8604 <rolikvia8@gmail.com>
2025-04-27 03:05:38 -0400

    RandomObstaclesDone

--- Assets/Scripts/UpdateScene.cs
+++ Assets/Scripts/UpdateScene.cs
@@ -28,6 +28,7 @@ public class UpdateScene : MonoBehaviour
     public void LoadNewerScene()
     {
         UpddateBackground();
+        UpdateObstacleArea();
 
     }
 
@@ -45,5 +46,56 @@ public class UpdateScene : MonoBehaviour
             backgroundImg.sprite = Resources.Load<Sprite>("Img/backgrounds/" + imgNumber);
         }
         
+    }
+
+    void UpdateObstacleArea()
+    {
+        List<GameObject> obstacles = LoadObstacles();
+        ClearOldObstacles();
+        SpawnNewObstacles(obstacles);
+    }
+
+    List<GameObject> LoadObstacles()
+    {
+        List<GameObject> obstacles = new List<GameObject>();
+        GameObject[] loadedPrefabs = Resources.LoadAll<GameObject>("Prefabs");
+        foreach (GameObject prefab in loadedPrefabs)
+        {
+            // Ignora los prefabs si su nombre es "and" o "or"
+            if (prefab.CompareTag("Obstacle") && prefab.name != "and" && prefab.name != "or")
+            {
+                obstacles.Add(prefab);
+            }
+        }
+        return obstacles;
+    }
+
+    void ClearOldObstacles()
+    {
+        if (obstaclesArea.transform.childCount != 0)
+        {
+            // Elimina los obstáculos viejos
+            foreach (Transform child in obstaclesArea.transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+
+    void SpawnNewObstacles(List<GameObject> obstacles)
+    {
+        Vector3 areaScale = obstaclesArea.transform.localScale;
+
+        // Carga 4 obstáculos nuevos seleccionados aleatoriamente
+        for (int i = 0; i < 4; i++)
+        {
+            int randomIndex = Random.Range(0, obstacles.Count);
+            GameObject newObstacle = Instantiate(obstacles[randomIndex], obstaclesArea.transform);
+
+            // Genera una posición aleatoria dentro del área
+            float randomX = Random.Range(-areaScale.x / 2, areaScale.x / 2);
+            float randomZ = Random.Range(-areaScale.z / 2, areaScale.z / 2);
+            newObstacle.transform.localPosition = new Vector3(randomX, 0, randomZ);
+        }
     }
 }
