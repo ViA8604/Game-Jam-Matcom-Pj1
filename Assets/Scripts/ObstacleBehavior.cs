8d2bc7321e29cf1387d9074a40b969101b06e6df
ViA8604 <rolikvia8@gmail.com>
2025-04-28 04:40:28 -0400

    BetterObstaclesDistribution

--- Assets/Scripts/ObstacleBehavior.cs
+++ Assets/Scripts/ObstacleBehavior.cs
@@ -6,12 +6,24 @@ public class ObstacleBehavior : MonoBehaviour
 {
     public ObstacleType obstacleType;
 
-
-    [SerializeField]
     public enum ObstacleType 
     {
         Mobile,
         Stable,
         Shooter
     };
+
+    void Update()
+    {
+        if (!IsVisible())
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private bool IsVisible()
+    {
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
+        return screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+    }
 }
